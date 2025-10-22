df567ccbfb818f9b80349f533b0918bdec87bcc6
khizermalik1 <khizermalik@hotmail.co.uk>
2025-10-22 11:10:21 +0100

    Initial feature build: custom models, styled homepage, responsive layout, login/logout setup

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    summary = models.CharField(max_length=300, blank=True)
+    ingredients = models.TextField()
+    steps = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.title)[:50]
+            slug = base
+            counter = 1
+            while Recipe.objects.filter(slug=slug).exists():
+                slug = f"{base}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.recipe}"
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,28 @@
+from django.views import generic
+from .models import Recipe
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "recipes/home.html")
+
+
+class RecipeListView(generic.ListView):
+    model = Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+    queryset = Recipe.objects.filter(is_published=True).order_by('-created_at')
+
+
+class RecipeDetailView(generic.DetailView):
+    model = Recipe
+    template_name = 'recipes/recipe_detail.html'
+
+
+class RecipeListView(generic.ListView):
+    model = Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+    queryset = Recipe.objects.filter(is_published=True).order_by('-created_at')
