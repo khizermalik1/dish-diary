0fe2da672396b63ef945f4e674c9bc8835bcda19
khizermalik1 <khizermalik@hotmail.co.uk>
2025-10-23 10:48:59 +0100

    enhance recipe deletion process with confirmation modal and clean up unused views

--- recipes/views.py
+++ recipes/views.py
@@ -3,14 +3,11 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 from django.http import HttpResponseForbidden
-from .models import Recipe
-from .forms import RecipeForm
-from django.db.models import Count
-from .models import Recipe, Comment, Like
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-
-
+from django.db.models import Count
+from .models import Recipe, Comment, Like
+from .forms import RecipeForm
 
 
 class RecipeListView(generic.ListView):
@@ -24,13 +21,12 @@ class RecipeListView(generic.ListView):
         context['popular_recipes'] = Recipe.objects.annotate(
             like_count=Count('likes')
         ).order_by('-like_count')[:4]
-        return context 
+        return context
 
 
 class RecipeDetailView(SuccessMessageMixin, generic.DetailView):
     model = Recipe
     template_name = 'recipes/recipe_detail.html'
-    
 
 
 def signup_view(request):
@@ -42,68 +38,6 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'recipes/signup.html', {'form': form})
-
-
-@login_required
-def create_recipe(request):
-    if request.method == 'POST':
-        form = RecipeForm(request.POST)
-        if form.is_valid():
-            recipe = form.save(commit=False)
-            recipe.author = request.user  # ✅ correct field
-            recipe.save()
-            return redirect('recipes:home')
-    else:
-        form = RecipeForm()
-    return render(request, 'recipes/create_recipe.html', {'form': form})
-
-
-@login_required
-def edit_recipe(request, slug):
-    recipe = get_object_or_404(Recipe, slug=slug)
-    if request.user != recipe.author and not request.user.is_superuser:
-        return HttpResponseForbidden("You can't edit this recipe.")
-
-    form = RecipeForm(request.POST or None, instance=recipe)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('recipes:home')
-
-    return render(request, 'recipes/edit_recipe.html', {'form': form})
-
-
-@login_required
-def delete_recipe(request, slug):
-    recipe = get_object_or_404(Recipe, slug=slug)
-    if request.user == recipe.author or request.user.is_superuser:
-        recipe.delete()
-        return redirect('recipes:home')
-    return HttpResponseForbidden("You can't delete this recipe.")
-
-
-@login_required
-def comment_recipe(request, slug):
-    recipe = get_object_or_404(Recipe, slug=slug)
-    if request.method == 'POST':
-        Comment.objects.create(
-            recipe=recipe,
-            author=request.user,
-            body=request.POST.get('body')
-        )
-    return redirect('recipes:recipe_detail', slug=slug)
-
-
-@login_required
-def like_recipe(request, slug):
-    recipe = get_object_or_404(Recipe, slug=slug)
-    Like.objects.get_or_create(recipe=recipe, user=request.user)
-    return redirect('recipes:recipe_detail', slug=slug)
-
-
-def latest_recipes(request):
-    page = int(request.GET.get('page', 1))
-    recipes = Recipe.objects.order_by('-created_at')[(page-1)*10:page*10]
-    return render(request, 'recipes/latest_recipes_chunk.html', {'recipes': recipes})
 
 
 @login_required
@@ -165,16 +99,9 @@ def like_recipe(request, slug):
     Like.objects.get_or_create(recipe=recipe, user=request.user)
     messages.success(request, "❤️ You liked this recipe!")
     return redirect('recipes:recipe_detail', slug=slug)
-from django.contrib.messages import get_messages
 
-class RecipeDetailView(generic.DetailView):
-    model = Recipe
-    template_name = 'recipes/recipe_detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['popular_recipes'] = Recipe.objects.annotate(
-            like_count=Count('likes')
-        ).order_by('-like_count')[:4]
-        context['messages'] = get_messages(self.request)
-        return context
+def latest_recipes(request):
+    page = int(request.GET.get('page', 1))
+    recipes = Recipe.objects.order_by('-created_at')[(page-1)*10:page*10]
+    return render(request, 'recipes/latest_recipes_chunk.html', {'recipes': recipes})
