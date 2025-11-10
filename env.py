e2641db08380de3718c27186bbfeea8a4544be01
khizermalik1 <khizermalik@hotmail.co.uk>
2025-11-10 10:59:09 +0000

    Remove sensitive and ignored files from Git tracking

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_xg5LGXieI2mN@ep-tiny-mouse-agx8ja3y.c-2.eu-central-1.aws.neon.tech/evil_sedan_mouse_619261"
-)
-os.environ.setdefault(
-    "SECRET_KEY", "&7k4@1(-b4433s!n%pf9wzg9r*b7yffd=mya@i@9j#5%=*u7$@")
-os.environ.setdefault("DEBUG", "True")
